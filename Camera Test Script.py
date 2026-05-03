d66383045a4ae23b315aec2390cdc9afd0f781e8
Dev Bohra <111324666+Dev-Bohra@users.noreply.github.com>
2026-05-03 20:36:52 +1000

    Simple Script for testing camera

---
+++ Camera Test Script.py
@@ -0,0 +1,20 @@
+import cv2
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+
+if not cap.isOpened():
+    print("❌ Camera not detected")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    cv2.imshow("Live Camera", frame)
+
+    # Press 'q' to close window
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
